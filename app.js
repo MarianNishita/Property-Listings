9b4ed9861b0abc45ad1effef825f064a12cdfc1b
MarianNishita <nishita.john7@gmail.com>
2018-03-04 16:58:21 +0530

    This is my last commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**
+ * app.js
+ * This files contains both the main module and controller for the Property-listings application
+ * All the functions are included in this file since it is a small application
+ * @version 0.5
+ * @author Marian John Bosco
+*/
 (function(){
 	var app = angular.module('property', [ ]);
 		//Controller defines a dependency to the $scope and the $http module
@@ -6,6 +13,7 @@
 			$http.get("results.json").success(function(data){
 				//the json data is assigned to $scope.results to make it available.
 				$scope.results = [];
+
 					angular.forEach(data.results, function(value, key){
 						$scope.results.push(value);
 					});
@@ -14,8 +22,32 @@
 					angular.forEach(data.saved, function(value,key){
 						$scope.saved.push(value);
 					})
+
+				// Return true if id is visible in the array
 				$scope.isVisible = function(id) {
 					return true;
+				};
+
+				//When hovered over listing displays the Add/Remove PropertyButton.
+				$scope.hoverIn = function(){
+					this.hoverEdit = true;
+				};
+
+				//When hovered out of listing hides the Add/Remove PropertyButton.
+				$scope.hoverOut = function(){
+					this.hoverEdit= false;
+				};
+
+				//Adds Property from result's list to saved list
+				$scope.addProperty = function(item){
+					$scope.saved.push(item);
+						$scope.results.splice($scope.results.indexOf(item), 1);
+				};
+
+				//Removes Property from saved list and moves back to Result's list (New functionality included to add property back to results list)
+				$scope.removeProperty = function(item){
+					$scope.results.push(item);
+						$scope.saved.splice($scope.saved.indexOf(item), 1);
 				};
 			});
 		});
